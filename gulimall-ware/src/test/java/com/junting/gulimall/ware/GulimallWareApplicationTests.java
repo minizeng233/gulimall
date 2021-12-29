c2a89098046da7fb94df5934b3c433b477969220
minizeng123 <1428087577@qq.com>
2021-12-29 11:45:29 +0800

    项目初始化结构提交

---
+++ gulimall-ware/src/test/java/com/junting/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junting.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
